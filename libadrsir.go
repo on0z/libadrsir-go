79d50d8507a496d5e7910846a98568e6d9a2739d
on0z <onoz.dev@gmail.com>
2022-03-12 17:34:30 +0900

    Sendコマンドを実装

--- libadrsir.go
+++ libadrsir.go
@@ -1,33 +1,39 @@
 package libadrsir
 
-const (
-	// ADRSIRのI2Cアドレス
-	ADDR = 0x52
+import (
+	"strconv"
 
-	// コマンド群
-	R1_memo_no_write  = 0x15
-	R2_data_num_read  = 0x25
-	R3_data_read      = 0x35
-	W2_data_num_write = 0x29
-	W3_data_write     = 0x39
-	T1_trans_start    = 0x59
+	"github.com/pkg/errors"
 )
 
-type bus interface {
+const (
+	// ADRSIRのI2Cアドレス
+	ADDR = byte(0x52)
+
+	// コマンド群
+	R1_memo_no_write  = byte(0x15)
+	R2_data_num_read  = byte(0x25)
+	R3_data_read      = byte(0x35)
+	W2_data_num_write = byte(0x29)
+	W3_data_write     = byte(0x39)
+	T1_trans_start    = byte(0x59)
+)
+
+type Bus interface {
 	ReadReg(byte, []byte) error
 	WriteReg(byte, []byte) error
 }
 
 type AdrsirAPI interface {
 	Get(index int) string
-	Send(irData string) error
+	Send(irCommandStr string) error
 }
 
 type adrsir struct {
-	bus bus
+	bus Bus
 }
 
-func NewADRSIR(device bus) AdrsirAPI {
+func NewADRSIR(device Bus) AdrsirAPI {
 	return &adrsir{
 		bus: device,
 	}
@@ -37,6 +43,40 @@ func (a *adrsir) Get(index int) string {
 	return ""
 }
 
-func (a *adrsir) Send(irData string) error {
+func (a *adrsir) Send(irCommandStr string) error {
+
+	// 文字列からbyte列を生成
+	var irCommand []byte
+	// 2文字づつ処理する
+	for i := 0; i < len(irCommandStr); i += 2 {
+		// 2文字を1バイトに変換する
+		aByte, err := strconv.ParseUint(irCommandStr[i:i+2], 16, 8)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		// 配列に追加
+		irCommand = append(irCommand, uint8(aByte))
+	}
+
+	// 送信予定のデータの長さを長さを書き込む
+	irCommandLength := uint16(len(irCommand))
+	sendData := []byte{byte(irCommandLength >> 8), byte(irCommandLength & 0xff)}
+
+	err := a.bus.WriteReg(W2_data_num_write, sendData)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	// irCommandを4バイトづつ書き込む
+	for i := 0; i < int(irCommandLength); i += 4 {
+		a.bus.WriteReg(W3_data_write, irCommand[i:i+4])
+	}
+
+	// transferコマンドを書き込む
+	err = a.bus.WriteReg(T1_trans_start, []byte{0})
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
 	return nil
 }
